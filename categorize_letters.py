1890a1ac48a40afd47d8241d27c21b6c0d1b3238
Appy <123403349+mvAA77@users.noreply.github.com>
2023-08-07 17:25:32 -0400

    Create categorize_letters.py

---
+++ categorize_letters.py
@@ -0,0 +1,7 @@
+let = input("Enter a Letter: ")
+num = int(let)
+
+if input == "a" or input == "e" or input == "i" or input == "o" or input == "u" or input == "A" or input == "E" or input == "I" or input == "O" or input == "U":
+  print("Vowel")
+else:
+  print("Consonant")
